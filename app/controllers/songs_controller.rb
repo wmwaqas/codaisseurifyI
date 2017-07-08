ccba7ee5870245a1579e1bac65868ecaf9e00352
wmwaqas <wmwaqasmahmood@hotmail.com>
2017-07-08 22:42:41 +0200

    Edit, update and delete songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,55 @@
 class SongsController < ApplicationController
 
+  def index
+     @songs = Song.all
+   end
+
   def new
     @song = Song.new
   end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    #song_params = params.require(:song).permit(:name, :album, :year, :rating)
+
+    @song = Song.new(song_params)
+
+    if @song.save
+       redirect_to @song
+    else
+       render 'new'
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+      @song = Song.find(params[:id])
+
+      #song_params = params.require(:song).permit(:name, :album, :year, :rating)
+
+      if @song.update_attributes(song_params)
+        redirect_to @song
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+    @song = Song.find(params[:id])
+
+    @song.destroy
+
+    redirect_to songs_path
+  end
+
+  private
+    def song_params
+      song_params = params.require(:song).permit(:name, :album, :year, :rating)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "artists#index"
+  root "songs#index"
 
-  get "artists" => "artists#index"
-  get 'artists/new' => "artists#new", as: :new_artist
-  get "artists/:id" => "artists#show", as: :artist
+  resources :artists
+  resources :songs
+
+  # get "artists" => "artists#index"
+  # get 'artists/new' => "artists#new", as: :new_artist
+  # get "artists/:id" => "artists#show", as: :artist
+  # get "songs" => "songs#index"
 end
